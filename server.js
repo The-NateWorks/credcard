595683fee911683d3d2aa589695ce5c0ebaba79c
The-NateWorks <lesternate6@outlook.com>
2025-12-10 07:53:19 -0600

    commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+var users = JSON.parse(fs.readFileSync("users.json","utf8"));
+
+var server = http.createServer((req, res) => {
+    if (req.url == "/buy") {
+        res.setHeader("Content-Type", "text/html");
+        res.end(fs.readFileSync("client.html", "utf8"));
+    }
+    res.on('data', (data) => {
+        console.log(data);
+    });
+})
+server.listen(3000, "0.0.0.0");
