85081ecf5949f566e533f5f3233a02264e437621
The-NateWorks <lesternate6@outlook.com>
2025-12-10 09:33:09 -0600

    commit

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
-var users = JSON.parse(fs.readFileSync("users.json","utf8"));
-
-var server = http.createServer((req, res) => {
-    if (req.url == "/buy") {
+const http = require('http');
+var server = http.createServer(function (req, res) {
+    if (req.url === '/buy') {
         res.writeHead(200, {'Content-Type': 'text/html', 'charset':'utf-8' });
         res.write(fs.readFileSync(path.join(__dirname, "client.html"), 'utf8'));
         res.end();
     }
-    res.on('data', (data) => {
-        console.log(data);
+    
+    req.on('data', (d) => {
+        console.log('Received data:', d.toString());
     });
-})
+});
 server.listen(3000, () => {
-    console.log("listen to me")
+    console.log('Server is listening on port 3000');
 });
