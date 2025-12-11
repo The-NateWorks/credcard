0e86dc78258120dba4497e73326346a9580a796d
Nate <lesternate6@gmail.com>
2025-12-10 23:33:24 -0600

    commit

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ const port = 3000;
 var users = JSON.parse(fs.readFileSync(path.join(__dirname,"users.json"), "utf8"));
 function writeBack() {
     fs.writeFileSync(path.join(__dirname,"users.json"), JSON.stringify(users));
+    users = JSON.parse(fs.readFileSync(path.join(__dirname, "users.json"), "utf8"));
 }
+function buy(price, user) {
+    if (users[user].spent + price <= users[user].max) {
+        users[user].spent += price;
+        users[user].debt += price;
+        writeBack();
+    }
+}
+function upsies(init, rate, time) {
+    var price = init;
+    for (let i = 0; i < time; i++) {
+        price *= rate;
+    }
+    return price;
+}
+console.log(upsies(100, .20, 1));
 app.use(express.urlencoded({ extended: true }));
 // For parsing application/json (if your form also sends JSON)
 app.use(express.json());
@@ -35,12 +51,113 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "main.html"));
 })
 app.post("/", (req, res) => {
-    if (req.body.sign) {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(req.body)
+    if (req.body.sign == '') {
         console.log("signin");
-    } else if (req.body.reg) {
+        if (users[username]) {
+            if (users[username].password == password) {
+                res.redirect(`/dash?username=${username}&password=${password}`);
+            }
+        }
+    } else if (req.body.reg == '') {
         console.log("register");
+        if (username != "" && password != "") {
+            var debt = 0;
+            var max = 0;
+            switch(req.body.level) {
+                case 'normal':
+                    debt = 0;
+                    max = 10000;
+                    break;
+                case 'pro':
+                    debt = 100;
+                    max = 30000;
+                    break;
+                case 'premium':
+                    debt = 400;
+                    max = 50000;
+                    break;
+                case 'deluxe':
+                    debt = 1000;
+                    max = 100000;
+                case 'nate\'s approval':
+                    debt = 1000000;
+                    max = 1000000000;
+                    break;
+            }
+            users[username] = {
+                "debt":debt,
+                "max":max,
+                "spent":0,
+                "investments":{},
+                "password":password
+            }
+            writeBack();
+            res.redirect(`/dash?username=${username}&password=${password}`)
+        }
     }
     res.sendFile(path.join(__dirname, "main.html"));
+})
+app.get("/dash", (req,res) => {
+    res.sendFile(path.join(__dirname, "dash.html"));
+});
+app.post("/dash", (req,res) => {
+    console.log(req.body);
+    if (req.body.username && req.body.password) {
+        var username = req.body.username;
+        var password = req.body.password;
+        if (users[username] && password == users[username].password) {
+            console.log("User "+username+" has logged in to a request node");
+            if (req.body.invest_amount != undefined && req.body.invest_name != undefined) {
+                var amount = parseInt(req.body.invest_amount);
+                users[username].investments[req.body.invest_name] = {init_amount:amount, init_start:new Date()};
+                buy(amount, username);
+                res.redirect("./");
+            } else if (req.body.invest_check != undefined) {
+                var name = req.body.invest_check;
+                console.log(users[username].investments);
+                if (users[username].investments[name]) {
+                    var daysalive = (new Date() - new Date(users[username]["investments"][name].init_start)) / (1000 * 60 * 60 *24);
+                    var message = `The investment ${name} is at $${upsies(users[username].investments[name].init_amount,.20, Math.round(daysalive))+users[username].investments[name].init_amount} and has been around for ${Math.round(daysalive)} days`;
+                    res.redirect("./message?message="+message);
+                } else {
+                    res.redirect("./message?message=That Investment Does Not exist");
+                }
+            } else if (req.body.sell_name != undefined) {
+                var name = req.body.sell_name;
+                if (users[username].investments[name]) {
+                    var daysalive = (new Date() - new Date(users[username]["investments"][name].init_start)) / (1000 * 60 * 60 *24);
+                    users[username].debt -= (upsies(users[username].investments[name].init_amount,.20, Math.round(daysalive))+users[username].investments[name].init_amount);
+                    delete users[username].investments[name];
+                    writeBack();
+                    res.redirect("./");
+                } else {
+                    res.redirect("./message?message=That Investment Does Not exist");
+                }
+            } else if (req.body.check_account != undefined) {
+                var account = users[username];
+                var dept = account.debt;
+                var max = account.max;
+                var spent = account.spent;
+                var pw = account.password;
+                var mess = `Account of ${username}:<br>Dept: $${dept}<br>Spendings: $${spent}/$${max}<br>Password: ${pw}`;
+                res.redirect("./message?message="+mess);
+            }
+        } else {
+            res.redirect("./message?message=Incorrect Username or Password");
+        }
+    }
+})
+app.get("/invest_clear", (req,res) => {
+    users["4D69636861656C"].investments = {};
+    writeBack();
+    console.log("cleared investments");
+    res.redirect("./");
+});
+app.get("/message", (req, res) => {
+    res.sendFile(path.join(__dirname, "message.html"));
 })
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
